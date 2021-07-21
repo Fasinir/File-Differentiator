e7ecad0b0947b372c7e60a20012a804542513def
Fasinir <filipson17@gmail.com>
2021-07-21 13:45:35 +0200

    extension matching

--- src/FileExtension.java
+++ src/FileExtension.java
@@ -1,8 +1,16 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
 public class FileExtension {
 
     private String name;
     private int offset;
     private int[][] magicCodes;
+    private int maxMagicLength;
+    private int minMagicLength;
 
     public FileExtension(String name, int offset, int[][] magicCodes)
     {
@@ -18,11 +26,44 @@ public class FileExtension {
         for (int i=0;i<magicCodes.length;i++)
         {
             this.magicCodes[i]=new int[magicStringCodes[i].length];
+            minMagicLength=Integer.MAX_VALUE;
             for (int j=0;j<magicCodes[i].length;j++)
             {
+                maxMagicLength=Math.max(maxMagicLength,magicCodes[i].length);
+                minMagicLength=Math.min(minMagicLength,magicCodes[i].length);
                 this.magicCodes[i][j]=Integer.decode(magicStringCodes[i][j]);
             }
         }
+    }
+    public boolean fileMatchesExtension(File file) throws IOException {
+        InputStream stream=new FileInputStream(file);
+        if(stream.skip(offset)!=offset)
+        {
+            stream.close();
+            return false;
+        }
+        int[] readCharacters=new int[maxMagicLength];
+        int idx=0;
+        while (stream.available()>0 && idx<maxMagicLength)
+        {
+            readCharacters[idx]=stream.read();
+            idx++;
+        }
+        if (idx>=minMagicLength)
+        {
+            for (int[] magicCode : magicCodes) {
+
+                int[] copy = Arrays.copyOfRange(readCharacters, 0, magicCode.length);
+                if(Arrays.equals(copy,magicCode))
+                {
+                    stream.close();
+                    return true;
+                }
+            }
+        }
+        stream.close();
+        return false;
+
     }
 
     public String getName() {
--- src/mainVoid.java
+++ src/mainVoid.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 public class mainVoid {
     public static void main(String[] args)
     {
+
         /*
         try
         {
-            File file = new File("examples/jpg1.jpg");
+            File file = new File("examples/jpg2.jpg");
             InputStream inputStream = new FileInputStream(file);
             printHexStream(inputStream,10);
         } catch (FileNotFoundException e)
@@ -20,6 +21,8 @@ public class mainVoid {
         }
 
          */
+
+
         FileExtension gifObj=new FileExtension("gif",0,
                 new String[][]{
                         {"0x47","0x49","0x46","0x38","0x37","0x61"},
@@ -32,19 +35,34 @@ public class mainVoid {
                                 {"0xff","0xd8","0xff","0xe0","0x00","0x10","0x4a","0x46","0x49","0x46","0x00","0x01",},
                                 {"0xff","0xd8","0xff","0xee",},
                         });
-        System.out.println(Arrays.deepToString(gifObj.getMagicCodes()));
-        System.out.println(Arrays.deepToString(jpgObj.getMagicCodes()));
+        //System.out.println(Arrays.deepToString(gifObj.getMagicCodes()));
+        //System.out.println(Arrays.deepToString(jpgObj.getMagicCodes()));
+        try
+        {
+            File file = new File("examples/jpg1.jpg");
+            File file2= new File("examples/jpg2.jpg");
+
+            System.out.println(jpgObj.fileMatchesExtension(file));
+            System.out.println(jpgObj.fileMatchesExtension(file2));
+            System.out.println(gifObj.fileMatchesExtension(file));
+            System.out.println(gifObj.fileMatchesExtension(file2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //helper function for testing
     public static void printHexStream(final InputStream inputStream, final int numberOfColumns) throws IOException {
         long streamPtr=0;
-        while (inputStream.available() > 0) {
+        int maxCount=32;
+        int count=0;
+        while (inputStream.available() > 0 && count<maxCount) {
             final long col = streamPtr++ % numberOfColumns;
             System.out.printf("%02x ",inputStream.read());
             if (col == (numberOfColumns-1)) {
                 System.out.printf("\n");
             }
+            count++;
         }
     }
 }
